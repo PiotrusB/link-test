c266310f6baf5c2261215227337b24f8341a73b1
PiotrusB <piotrpawelbozek@gmail.com>
2025-05-23 16:43:21 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+background(0);
+}
+
+function mousePressed() {
+window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+}
